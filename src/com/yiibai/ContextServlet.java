ed49716bf6927b8c5d69bef7641197ec546725f2
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-18 12:00:25 +0800

    Initial commit

---
+++ src/com/yiibai/ContextServlet.java
@@ -0,0 +1,40 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextServlet
+ */
+@WebServlet("/ContextServlet")
+public class ContextServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+
+        // creating ServletContext object
+        ServletContext context = getServletContext();
+
+        // Getting the value of the initialization parameter and printing it
+        String driverName = context.getInitParameter("dname");
+        pw.println("driver name is = <b>'" + driverName+"'</b>");
+
+        pw.close();
+    }
+
+}
